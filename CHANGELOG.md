edf95c1b4806ccb6f6b77c0d0a12b16dd4b0e88e
sportshead <32637656+sportshead@users.noreply.github.com>
2021-12-26 10:14:49 -0800

    initial

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+<!-- For torn forums: https://ddormer.github.io/markdown-to-bbcode-site/ -->
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.0.1] 2021-12-26
+### Added
+- Fetch interceptor
+- Hook/unhook system
+- Buttons on beer tent
+- Automatic bucks updating
---
+++ beer_glasses_PROD.user.js
@@ -0,0 +1,80 @@
+// ==UserScript==
+// @name         Torn: Gimme beer glasses
+// @namespace    dev.sportshead.gimme_beer_glasses
+// @version      0.0.1
+// @description  Gimme beer glasses!
+// @author       sportzpikachu
+// @match        https://www.torn.com/christmas_town.php*
+// @grant        unsafeWindow
+// ==/UserScript==
+// Based on https://github.com/f2404/torn-userscripts/raw/master/gimme_beers.user.js by lugburz
+
+(function (window, $, getAction) {
+    'use strict';
+    console.log('beer glasses run');
+    const button = `<button id="buyBeerBtn" style="color: var(--default-blue-color); cursor: pointer; margin-right: 0;">Gimme beer glasses!</button>
+    <span id="buyBeerResult" style="font-size: 12px; font-weight: 100;"></span>`;
+    let active = false;
+    const originalFetch = window.fetch;
+    window.fetch = async (...args) => {
+        console.log('fetch intercepted');
+        const response = await originalFetch(...args); if (response.url.includes('christmas_town.php?q=miniGameAction') && args[1].body.includes('{"gameType":"gameGiftShop","action":"getItems"}')) {
+            console.log('fetch gameGiftShop')
+            const res = await response.clone().json();
+            if (res.shopType === 'Beer tent' && !active) {
+                console.log('fetch beer tent');
+                setTimeout(() => {
+                    hook(button);
+                    active = true;
+                }, 100);
+            }
+        } else if (active && response.url.includes('christmas_town.php?q=move')) {
+            console.log('fetch move');
+            unHook();
+            active = false;
+        }
+
+        return response;
+    };
+
+    function hook(button) {
+        console.log('beer glasses hook');
+        if ($('#buyBeerBtn').size() < 1) {
+            $('div.status-title > div').before(button);
+            $('#buyBeerBtn').on('click', async () => {
+                $('#buyBeerResult').text('');
+                const obj = await $.ajax({
+                    type: 'post',
+                    url: 'christmas_town.php?q=miniGameAction',
+                    contentType: 'application/json; charset=UTF-8',
+                    data: JSON.stringify({
+                        gameType: 'gameGiftShop',
+                        action: 'buyItem',
+                        result: {
+                            giftShopType: 'basic',
+                            itemType: 816,
+                            itemCategory: 'tornItems'
+                        }
+                    })
+                });
+                if (obj.success === false) {
+                    console.log('beer glasses error', obj);
+                    $('#buyBeerResult').text(obj.error).css('color', 'red');
+                } else if (obj.userData.userStatus === "ok") {
+                    console.log('beer glasses bought');
+                    $('#buyBeerResult').text('Added to your items.').css('color', 'green');
+                    $('div.status-title > div > div > span > span').text($('ul.items-list > li.bucks > span.quantity').text());
+                } else {
+                    console.log('beer glasses error', obj);
+                    $('#buyBeerResult').text(!!obj.userData.message.trim() ? obj.userData.message : 'Something went wrong.').css('color', 'red');
+                }
+            });
+        }
+    }
+
+    function unHook() {
+        console.log('beer glasses unhook');
+        $('#buyBeerBtn').remove();
+        $('#buyBeerResult').remove();
+    }
+})(unsafeWindow, unsafeWindow.jQuery, unsafeWindow.getAction);
